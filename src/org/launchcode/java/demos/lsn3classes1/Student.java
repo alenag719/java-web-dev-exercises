6e8aa58821a7757c827c691256dae3947467df26
alenag719 <alenag719@gmail.com>
2023-01-17 15:45:58 -0800

    completed

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -5,9 +5,34 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class Student {
 
-    private String name;
+    private String name = "Alena Gladkova";
     private int studentId;
-    private int numberOfCredits = 0;
-    private double gpa = 0.0;
+    private int numberOfCredits = 1;
+    private double gpa = 4.0;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getStudentId() {
+        return studentId;
+    }
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+    public double getGpa() {
+        return gpa;
+    }
 
 }
