8f5e1d1c405060eee6c74a05e0285c03dbf7afd2
alenag719 <alenag719@gmail.com>
2023-01-17 13:52:43 -0800

    completed

---
+++ src/exercises/ArrayListPractice.java
@@ -0,0 +1,22 @@
+package exercises;
+
+import org.jetbrains.annotations.Contract;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ArrayListPractice {
+    @Contract(pure = true)
+    public static int sumEven(ArrayList<Integer> arr) {
+        int total = 0;
+        for (int integer : arr) {
+            if (integer % 2 == 0) {
+                total += integer;
+            }
+        }
+        return total;
+    }
+        public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word length: ");
+        int numChars = input.nextInt();
+    }
+}
---
+++ src/exercises/ArrayPractice.java
@@ -0,0 +1,14 @@
+package exercises;
+
+import java.util.Arrays;
+
+public class ArrayPractice {
+
+    public static void main(String[] args) {
+
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+        for (int i : integerArray) {
+            System.out.println(i);
+        }
+    }
+}
